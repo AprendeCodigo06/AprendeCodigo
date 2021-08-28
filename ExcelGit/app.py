b1232ca5156819bd057eca637a7cb4baa41edac9
AprendeCodigo06 <robdev060@gmail.com>
2021-08-28 12:26:28 -0600

    Se agrego el proyecto

--- ExcelGit/app.py
+++ ExcelGit/app.py
@@ -1,9 +1,8 @@
 import pandas as pd
-archivo = 'Filtrar Excel\libro.xlsx'
-datos = pd.read_excel(archivo)
-print("Sin filtro:\n",datos.head(10))
+archivo  =  'ExcelGit\libro.xlsx'
+datos  =  pd . read_excel ( archivo )
+print ( "Sin filtro: \ n " , datos . head ( 10 ))
 
-datos = datos[datos["Universidad"] == "itcj"]
+datos  =  datos [ datos [ "Universidad" ] ==  "itcj" ]
 
-print("Con filtro:\n", datos.head(10))
-
+print ( "Con filtro: \ n " , datos . head ( 10 ))
